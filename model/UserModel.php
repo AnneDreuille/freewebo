44556e2b600907fbbfc53b9a8a908b273dbd8cca
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-03-06 15:03:11 +0100

    Create UserModel.php

---
+++ model/UserModel.php
@@ -0,0 +1,65 @@
+<?php
+require_once(__DIR__.'/Model.php');
+
+class UserModel extends Model {
+
+//insérer un nouveau user
+  public function signUp($userType,$firstName,$lastName,$mail,$phone,$password)
+    $db= $this->dbConnect();
+
+    $req = $db->prepare('INSERT INTO user (userType,firstName,lastName,mail,phone,password,signUpDate) VALUES (?,?,?,?,?,?,NOW())');
+
+    return $req->execute(array($userType,$firstName,$lastName,$mail,$phone,$password));
+  }
+
+  //récupérer le nombre de clients
+    public function nbClient() {
+      $db= $this->dbConnect();
+
+      $req = $db->prepare('SELECT COUNT(id) AS nbClient FROM user WHERE userType="client"');
+
+      $req->execute();
+      return $req->fetchColumn();
+    }
+
+  //récupérer le nombre de développeurs
+    public function nbDev() {
+      $db= $this->dbConnect();
+
+      $req = $db->prepare('SELECT COUNT(id) AS nbDev FROM user WHERE userType="dev"');
+
+      $req->execute();
+      return $req->fetchColumn();
+    }
+
+  //récupérer la liste des clients
+    public function listClient() {
+      $db= $this->dbConnect();
+
+      $req = $db->prepare('SELECT id,firstName,lastName,mail,phone,DATE_FORMAT(signUpDate, "%d/%m/%Y à %Hh%i") AS signUpDate_fr, DATE_FORMAT(blacklistDate, "%d/%m/%Y à %Hh%i") AS blacklistDate_fr FROM user ORDER BY signUpDate ASC ');
+
+      $req->execute();
+      return $req->fetchAll();
+    }
+
+//récupérer la liste des développeurs
+    public function listDev() {
+      $db= $this->dbConnect();
+
+      $req = $db->prepare('SELECT id,firstName,lastName,mail,phone,DATE_FORMAT(signUpDate, "%d/%m/%Y à %Hh%i") AS signUpDate_fr, DATE_FORMAT(blacklistDate, "%d/%m/%Y à %Hh%i") AS blacklistDate_fr FROM user ORDER BY signUpDate ASC ');
+
+      $req->execute();
+      return $req->fetchAll();
+    }
+
+//blacklister 1 user
+  public function blacklist($blacklistDate, $id) {
+    $db= $this->dbConnect();
+
+    $req = $db->prepare('UPDATE user SET blacklistDate=NOW() WHERE id=?');
+
+    return $req->execute(array($id));
+  }
+
+
+}
