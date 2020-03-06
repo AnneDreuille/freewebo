7a15b6b02d61da50607147627b4e6af65c696179
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-03-06 15:37:35 +0100

    Create ChatModel.php

---
+++ model/ChatModel.php
@@ -0,0 +1,26 @@
+<?php
+require_once(__DIR__.'/Model.php');
+
+class ChatModel extends Model {
+
+//écrire un nouveau message
+  public function addMessage($idSender,$idProject,$message)
+    $db= $this->dbConnect();
+
+    $req = $db->prepare('INSERT INTO chat (idSender,idProject,message,postDate) VALUES (?,?,?,NOW())');
+
+    return $req->execute(array($idSender,$idProject,$message);
+  }
+
+//récupérer la liste des messages
+    public function listMessage() {
+      $db= $this->dbConnect();
+
+      $req = $db->prepare('SELECT id,idSender,idProject,message,DATE_FORMAT(postDate, "%d/%m/%Y à %Hh%i") AS postDate_fr FROM chat ORDER BY postDate DESC');
+
+      $req->execute();
+      return $req->fetchAll();
+    }
+
+
+}
