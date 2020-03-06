a229caf3b09bac63fdf4d7d84f7afe860ce35417
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-03-06 13:28:21 +0100

    Create Model.php

---
+++ model/Model.php
@@ -0,0 +1,9 @@
+<?php
+
+class Model {
+  //se connecter à MySQL avec l'extension PDO (PHP Data Objects)+ tableau erreurs SQL
+  protected function dbConnect() {
+    $db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8',DB_USER,DB_PSWD, array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION));
+      return $db;
+  }
+}
