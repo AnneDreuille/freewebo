cbd23b7834c0cc79c4faab368a53db665d61b303
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-03-09 17:13:36 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+//charger le fichier config avec définition des constantes d'accès à la db
+require_once(__DIR__.'/app/config.php');
+
+//charger les fichiers controller
+// require_once(__DIR__.'/controller/frontController.php');
+// require_once(__DIR__.'/controller/backController.php');
+
+//tester le paramètre action pour savoir quelle fonction du controleur appeler
+try {
+
+  if (isset($_GET['action'])) {
+
+    if ($_GET['action']=='signUp') {
+      signUp();
+    }
+
+    // elseif ($_GET['action']=='') {
+    //   ();
+    // }
+
+    // elseif($_GET['action']=='') {
+    //  ();
+    // }
+
+    // elseif($_GET['action']=='') {
+    //   ();
+    // }
+  }
+  //afficher par défaut la liste des posts publiés : page d'accueil listPosts
+  else {
+    homepage();
+  }
+}
+catch(Exception $e) {
+  echo 'Erreur : ' .$e->getMessage();
+}
