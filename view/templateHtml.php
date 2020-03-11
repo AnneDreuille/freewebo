17c18c3b0117745b5e689ab34245d8deea0ec270
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-03-11 19:02:03 +0100

    Update templateHtml.php
    modif liens script bootstrap jquery

--- view/templateHtml.php
+++ view/templateHtml.php
@@ -22,7 +22,7 @@
         <link rel="icon" href="/freewebo/public/images/favicon.ico" type="image/ico" />
 
         <!--lien CDN bootstrap-->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
         <!--lien autre style-->
         <link rel="stylesheet" href="/freewebo/public/style.css"/>
 
@@ -79,11 +79,9 @@
         <script>tinymce.init({ selector:'textarea'});</script>
 
         <!-- script pour bootstrap, jquery & js-->
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
-
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
         <!-- <script src="/freewebo/public/main.js"></script> -->
 
