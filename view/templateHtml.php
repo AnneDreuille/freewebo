ce1a61b5b289f7e8e2c313c7224127e795e53688
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-03-12 18:15:07 +0100

    Update templateHtml.php
    ajout script bootstrap show password
    ajout lien main.js

--- view/templateHtml.php
+++ view/templateHtml.php
@@ -80,10 +80,12 @@
 
         <!-- script pour bootstrap, jquery & js-->
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-        <!-- <script src="/freewebo/public/main.js"></script> -->
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://unpkg.com/bootstrap-show-password@1.2.1/dist/bootstrap-show-password.min.js"></script>
+
+        <script src="/freewebo/public/main.js"></script>
 
         <hr/><br/>
 
