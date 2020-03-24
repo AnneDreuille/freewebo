dd086218e23d0f42d2194aebcee2e7753c1e2799
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-03-24 15:49:59 +0100

    Update backController.php
    modif project & assign

--- controller/backController.php
+++ controller/backController.php
@@ -24,38 +24,52 @@ function admin(){
 //afficher les données d'un projet
 function project(){
 
-    //créer l'objet
-    $projectModel = new ProjectModel();
+    //vérifier qu'on a bien reçu un id en paramètre dans l'url
+    if (isset($_GET['id']) && $_GET['id']>0) {
 
-    //appeler la fonction de cet objet
-    $project= $projectModel->project($_GET['id']);
+        //créer l'objet
+        $projectModel = new ProjectModel();
 
-    //charger le fichier en vue de l'affichage dans la page html
-    require(__DIR__.'/../view/back/project.php');
-}
-
-//nommer 1 dév sur un projet
-function assign(){
-
-    //vérifier que le formulaire a bien reçu les paramètres
-    if (!empty($_POST['mail'])) {
-
-        //créer les objets
-        $userModel= new UserModel();
-        $projectModel= new ProjectModel();
-
-        //appeler les fonctions de ces objets
-        $member= $userModel->signIn($_POST['mail']);
-
-        $idDev= $member['id'];
-
-        $assign= $projectModel->assign($idDev);
+        //appeler la fonction de cet objet
+        $project= $projectModel->project($_GET['id']);
 
         //charger le fichier en vue de l'affichage dans la page html
         require(__DIR__.'/../view/back/project.php');
 
     } else {
+        throw new Exception('Erreur : pas de projet identifié');
+    }
+}
+
+//nommer 1 dév sur un projet
+function assign(){
+
+    //vérifier qu'on a un id projet dans l'url
+    if (isset($_GET['id']) && $_GET['id']>0) {
+
+        //vérifier qu'on a le mail dev dans le formulaire
+        if (!empty($_POST['mail'])) {
+
+            //créer les objets
+            $userModel= new UserModel();
+            $projectModel= new ProjectModel();
+
+            //appeler les fonctions de ces objets
+            $member= $userModel->signIn($_POST['mail']);
+
+            $idDev= $member['id'];
+
+            $assign= $projectModel->assign($idDev, $_GET['id']);
+
+            //charger le fichier en vue de l'affichage dans la page html
+            require(__DIR__.'/../view/back/project.php');
+
+        } else {
+            //envoyer une exception dans catch en cas d'erreur
+            throw new Exception('Pas de développeur identifié');
+        }
+    } else {
         //envoyer une exception dans catch en cas d'erreur
-        throw new Exception('Pas de développeur identifié');
+        throw new Exception('Pas de projet identifié');
     }
 }
