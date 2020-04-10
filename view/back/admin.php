842c59e588e37dea85f379e18615befc63779a8b
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-04-10 10:01:11 +0200

    Update admin.php
    ajout ratingDev ds liste projets terminés

--- view/back/admin.php
+++ view/back/admin.php
@@ -119,9 +119,9 @@
             <div class="col-md-4"></div>
         </div><br/>
 
-        <!-- liste PROJETS TERMINES-->
         <div class="row">
-            <div class="offset-sm-6 col-sm-6">
+            <!-- liste PROJETS TERMINES-->
+            <div class="offset-sm-2 col-sm-8">
                 <div class="table-responsive-sm border">
                     <p class= "text-info text-center font-weight-bold">LISTE DES PROJETS TERMINES</p>
                     <table class="table table-striped table-condensed small">
@@ -131,6 +131,7 @@
                                 <th>Nom projet</th>
                                 <th>idClient</th>
                                 <th>idDev</th>
+                                <th>ratingDev</th>
                                 <th class="text-center">suivi projet</th>
                                 <th class="text-center">espace membre</th>
                             </tr>
@@ -140,12 +141,12 @@
                         foreach ($listProject as $data) {
                             if (!empty($data['endDate_fr'])) {
                         ?>
-
                             <tr>
                                 <td><?php echo htmlspecialchars($data['id']); ?></td>
                                 <td><?php echo htmlspecialchars($data['name']); ?></td>
                                 <td><?php echo htmlspecialchars($data['idClient']); ?></td>
                                 <td><?php echo htmlspecialchars($data['idDev']); ?></td>
+                                <td><?php echo htmlspecialchars($data['ratingDev']); ?></td>
                                 <td class="text-center"><a href="index.php?action=project&id=<?php echo htmlspecialchars($data['id']); ?>" role="button" class="fas fa-share btn-info"></a></td>
                                 <td class="text-center"><a href="index.php?action=member&id=<?php echo htmlspecialchars($data['id']); ?>" role="button" class="fas fa-share btn-success"></a></td>
                             </tr>
@@ -166,7 +167,7 @@
         </div>
 
         <!-- Bouton back to the top -->
-        <a href="#" class="fixed-action-btn smooth-scroll btn-floating btn-lg btn-info rounded-circle float-right"><span class="fas fa-arrow-circle-up"></span></a>
+        <a href="#" class="fixed-action-btn smooth-scroll btn-floating btn-lg btn-info rounded-circle float-right"><span class="fas fa-arrow-circle-up"></span></a><br/><br/>
 
     </div><br/><br/> <!-- fin container -->
 
