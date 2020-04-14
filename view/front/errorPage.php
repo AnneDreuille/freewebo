c7a3eeba560be2ad41395a3202bd7d4204454ee4
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-04-14 10:43:34 +0200

    Update errorPage.php
    modif texte intro
    modif btn retour esp membre : ajout condit if pour user non inscrit

--- view/front/errorPage.php
+++ view/front/errorPage.php
@@ -9,7 +9,7 @@
             <div class="col-md-3"></div>
 
             <div class="col-md-6">
-                <p class="text-center bg-info rounded text-white py-3 h5">Oups ! vous vous êtes perdu(e).<br/> Pas de souci : vous pouvez revenir à la page d'accueil<br/> ou dans votre espace membre.</p>
+                <p class="text-center bg-info rounded text-white py-3 h5">Oups ! vous vous êtes perdu(e).<br/> Pas de souci : vous pouvez revenir à la page d'accueil<br/> ou dans votre espace membre si vous êtes incrit(e).</p>
             </div>
             <div class="col-md-3"></div>
         </div>
@@ -26,22 +26,23 @@
 
 
         <!-- bouton Retour à l'espace membre-->
-        <?php if (!empty($_SESSION ['userType']) && $_SESSION ['userType']==="admin"){
-            echo '';
-            } else {?>
+        <?php if (!empty($_SESSION ['userType']) && $_SESSION ['userType']==="admin") {
+            if (empty($_SESSION ['userType'])) {
+                echo '';
+                } else {?>
         <div class="row">
             <div class='col-md-3'>
                 <a class="btn btn-lg btn-success btn-sm" href="index.php?action=member" role="button"><span class="fas fa-campground pr-1"></span>Retour à l'espace membre</a>
             </div>
-        </div>
-        <?php }?>
+        </div><br/>
+        <?php }}?>
 
         <!-- bouton Retour à la page d'accueil-->
         <div class="row">
             <div class='col-md-4'>
                 <a class="btn btn-lg btn-info btn-sm" href="index.php" role="button"><span class="fas fa-home pr-1"></span>Retour à la page d'accueil</a>
             </div>
-        </div>
+        </div><br/>
 
         <!-- Bouton back to the top -->
         <a href="#" class="fixed-action-btn smooth-scroll btn-floating btn-lg btn-info rounded-circle float-right"><span class="fas fa-arrow-circle-up"></span></a><br/><br/>
