7fff2365a5db85fd055cccf3ee5e26238f1441be
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-04-15 17:10:19 +0200

    Update need.php
    php syntaxe alternative

--- view/front/need.php
+++ view/front/need.php
@@ -61,17 +61,13 @@
 
 
         <!-- bouton Retour à l'espace membre-->
-        <?php if (!empty($_SESSION ['userType']) && $_SESSION ['userType']==="admin") {
-              echo '';
-              } elseif (empty($_SESSION ['userType'])) {
-                echo '';
-                } else {?>
+        <?php if (!empty($_SESSION ['userType']) && $_SESSION ['userType']!=="admin") :?>
         <div class="row">
             <div class='col-md-3'>
                 <a class="btn btn-lg btn-success btn-sm" href="index.php?action=member" role="button"><span class="fas fa-campground pr-1"></span>Retour à l'espace membre</a>
             </div>
         </div>
-        <?php }?>
+        <?php endif;?>
 
         <!-- Bouton back to the top -->
         <a href="#" class="fixed-action-btn smooth-scroll btn-floating btn-lg btn-info rounded-circle float-right"><span class="fas fa-arrow-circle-up"></span></a><br/><br/>
