eb9dbf3ad3ff9c814479308257a9f2e9416c2527
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-04-16 15:43:18 +0200

    Update main.js
    commenter code provisoire

--- public/main.js
+++ public/main.js
@@ -2,21 +2,21 @@
 
 //détecter validation formulaire
 
-$('.submit').submit (function (event) {
-  event.preventDefault(); //permet de ne pas recharger la page
+// $('.submit').submit (function (event) {
+//   event.preventDefault(); //permet de ne pas recharger la page
 
-  let $form=$(this); //this = formulaire sur lequel on a cliqué
+//   let $form=$(this); //this = formulaire sur lequel on a cliqué
 
-  //requête ajax
-  $.ajax({
-    url:$form.attr('action'), //attr renvoie à l'attribut action
-    data:new FormData($form[0]), //objet avec arg.qui fait réf à form html
-    method:"post"
+//   //requête ajax
+//   $.ajax({
+//     url:$form.attr('action'), //attr renvoie à l'attribut action
+//     data:new FormData($form[0]), //objet avec arg.qui fait réf à form html
+//     method:"post"
 
-  //afficher msg lors de l'évènement
-  }) .done(function(){ //dès que la requête a réussi
+//   //afficher msg lors de l'évènement
+//   }) .done(function(){ //dès que la requête a réussi
 
-    $form.find('p#msg').text('Votre description de besoin a bien été enregistrée.');
-  })
-});
+//     $form.find('p#msg').text('Votre description de besoin a bien été enregistrée.');
+//   })
+// });
 
