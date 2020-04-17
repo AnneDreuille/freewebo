1439e0092cf36d9b9a6c35148ab4cd65512c5084
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-04-17 16:11:47 +0200

    Update ChatModel.php
    ajout listMessageDev

--- model/ChatModel.php
+++ model/ChatModel.php
@@ -13,7 +13,7 @@ class ChatModel extends Model {
     return $req->execute(array($idSender,$idProject,$message));
   }
 
-//récupérer la liste des messages
+//récupérer la liste des messages d'un projet
     public function listMessage($idProject) {
       $db= $this->dbConnect();
 
@@ -25,6 +25,20 @@ class ChatModel extends Model {
         ORDER BY postDate DESC ');
 
       $req->execute(array($idProject));
+      return $req->fetchAll();
+    }
+
+//récupérer la liste des messages DEV
+    public function listMessageDev() {
+      $db= $this->dbConnect();
+
+      $req = $db->prepare('SELECT user.firstName, chat.id, chat.idProject, chat.message, DATE_FORMAT(chat.postDate, "%d/%m/%Y à %Hh%i") AS postDate_fr
+        FROM chat
+        INNER JOIN user
+        ON user.id=chat.idSender
+        ORDER BY postDate DESC ');
+
+      $req->execute();
       return $req->fetchAll();
     }
 
