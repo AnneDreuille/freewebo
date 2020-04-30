2510fa0b44013347d34bc52e2d6aeafb07190a3a
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-04-30 15:02:34 +0200

    Update main.js
    réindation à 4
    modif class submitNeed
    ajout req Ajax sur submitPost

--- public/main.js
+++ public/main.js
@@ -1,23 +1,44 @@
-//jquery pour afficher le message de succès du formulaire need
+//AFFICHER LE MESSAGE DE SUCCES DU FORMULAIRE NEED
 
 //détecter validation formulaire
-$('.submit').submit (function (event) {
-  event.preventDefault(); //permet de ne pas recharger la page
-  tinyMCE.triggerSave(); //enregistre le contenu de texterea
-  let $form=$(this); //this = formulaire sur lequel on a cliqué
+$('.submitNeed').submit (function (event) {
+    event.preventDefault(); //permet de ne pas recharger la page
+    tinyMCE.triggerSave(); //enregistre le contenu de texterea
+    let $form=$(this); //this = formulaire sur lequel on a cliqué
 
-  //requête ajax
-  $.ajax({
-    url:$form.attr('action'), //attr renvoie à l'attribut... action
-    data:new FormData($form[0]), //objet avec arg.qui fait réf à form html
-    processData: false, //indique à jQuery de ne pas traiter les données
-    contentType: false, //indique à jQuery de ne pas configurer le contentType
-    method:"post"
+    //requête ajax
+    $.ajax({
+        url:$form.attr('action'), //attr renvoie à l'attribut... action
+        data:new FormData($form[0]), //objet avec arg.qui fait réf à form html
+        processData: false, //indique à jQuery de ne pas traiter les données
+        contentType: false, //indique à jQuery de ne pas configurer le contentType
+        method:"post"
 
-  //afficher msg lors de l'évènement
-  }) .done(function(){ //dès que la requête ajax a réussi
+    //afficher msg lors de l'évènement
+    }) .done(function(){ //dès que la requête ajax a réussi
 
-    $form.find('p#msg').text('Votre description de besoin a bien été enregistrée.');
-  })
+        $form.find('p#msg').text('Votre description de besoin a bien été enregistrée.');
+    })
 });
 
+//ACTUALISER LES MESSAGES POSTES PAR PLUSIEURS USERS
+
+//détecter validation formulaire
+$('.submitPost').submit (function (event) {
+    event.preventDefault(); //permet de ne pas recharger la page
+    tinyMCE.triggerSave(); //enregistre le contenu de texterea
+    let $form=$(this); //this = formulaire sur lequel on a cliqué
+
+    //requête ajax
+    $.ajax({
+        url:$form.attr('action'), //attr renvoie à l'attribut... action
+        data:new FormData($form[0]), //objet avec arg.qui fait réf à form html
+        processData: false, //indique à jQuery de ne pas traiter les données
+        contentType: false, //indique à jQuery de ne pas configurer le contentType
+        method:"post"
+
+    //actualiser les msg lors de l'évènement
+    // }) .done(function(){ //dès que la requête ajax a réussi
+
+    })
+});
