37acd6e51e3c3c82cb31de72c10ff33b1e345f34
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-05-04 16:47:58 +0200

    Update main.js
    modif présentation comments => au dessus code

--- public/main.js
+++ public/main.js
@@ -23,27 +23,38 @@ $('.submitNeed').submit (function (event) {
 
 //ACTUALISER LES MESSAGES POSTES PAR PLUSIEURS USERS
 
-//détecter validation formulaire
+//détecter validation formulaire avec classe submitPost
 $('.submitPost').submit (function (event) {
-    event.preventDefault(); //permet de ne pas recharger la page
-    tinyMCE.triggerSave(); //enregistre le contenu de texterea
-    let $form=$(this); //this = formulaire sur lequel on a cliqué
+    //ne pas recharger la page
+    event.preventDefault();
+    //enregistrer le contenu de texterea
+    tinyMCE.triggerSave();
+    //créer la var form avec this = form. sur lequel on a cliqué
+    let $form=$(this);
 
     //requête ajax
     $.ajax({
-        url:$form.attr('action'), //attr renvoie à l'attribut... action
-        data:new FormData($form[0]), //objet avec arg.qui fait réf à form html
-        processData: false, //indique à jQuery de ne pas traiter les données
-        contentType: false, //indique à jQuery de ne pas configurer le contentType
+        //url du form. avec attr = attribut action
+        url:$form.attr('action'),
+        //objet avec arg.qui fait réf à form html
+        data:new FormData($form[0]),
+        //ne pas traiter les données
+        processData: false,
+        //ne pas configurer le contentType
+        contentType: false,
         method:"post",
         dataType:"JSON"
 
     //actualiser les msg lors de l'évènement
-    }) .done(function(data){ //dès que la requête ajax a réussi
+    //dès que la requête ajax a réussi
+    }) .done(function(data){
         if (data.success===true){
-            let listMessage= $("#listMessage"); //réf à id listMessage
-            listMessage.html(""); //met à 0 les msg précédents
+            //réf à id listMessage
+            let listMessage= $("#listMessage");
+            //met à 0 les msg précédents
+            listMessage.html("");
             $("#message").val(""); //à revoir
+            //afficher les msg postés
             data.messages.forEach(function(element) {
                 listMessage.append(
                     `<dl>
