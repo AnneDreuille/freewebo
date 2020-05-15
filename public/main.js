2a85ca71140dc9e1bc3300a0e00ecb79edab905c
Anne Dreuille <anne.m.dreuille@gmail.com>
2020-05-15 16:28:06 +0200

    Update main.js
    réorga ordre code
    réorga comments

--- public/main.js
+++ public/main.js
@@ -1,26 +1,3 @@
-//AFFICHER LE MESSAGE DE SUCCES DU FORMULAIRE NEED
-
-//détecter validation formulaire
-$('.submitNeed').submit (function (event) {
-    event.preventDefault(); //permet de ne pas recharger la page
-    tinyMCE.triggerSave(); //enregistre le contenu de texterea
-    let $form=$(this); //this = formulaire sur lequel on a cliqué
-
-    //requête ajax
-    $.ajax({
-        url:$form.attr('action'), //attr renvoie à l'attribut... action
-        data:new FormData($form[0]), //objet avec arg.qui fait réf à form html
-        processData: false, //indique à jQuery de ne pas traiter les données
-        contentType: false, //indique à jQuery de ne pas configurer le contentType
-        method:"post"
-
-    //afficher msg lors de l'évènement
-    }) .done(function(){ //dès que la requête ajax a réussi
-
-        $form.find('p#msg').text('Votre description de besoin a bien été enregistrée.');
-    })
-});
-
 //ACTUALISER LES MESSAGES POSTES PAR PLUSIEURS USERS
 
 //détecter validation formulaire avec classe submitPost
@@ -29,7 +6,7 @@ $('.submitPost').submit (function (event) {
     event.preventDefault();
     //enregistrer le contenu de texterea
     tinyMCE.triggerSave();
-    //créer la var form avec this = form. sur lequel on a cliqué
+    //this = formulaire sur lequel on a cliqué
     let $form=$(this);
 
     //requête ajax
@@ -66,4 +43,35 @@ $('.submitPost').submit (function (event) {
             });
         }
     });
+});
+
+//AFFICHER LE MESSAGE DE SUCCES DU FORMULAIRE NEED
+
+//détecter validation formulaire
+$('.submitNeed').submit (function (event) {
+    //ne pas recharger la page
+    event.preventDefault();
+    //enregistrer le contenu de texterea
+    tinyMCE.triggerSave();
+    //this = formulaire sur lequel on a cliqué
+    let $form=$(this);
+
+    //requête ajax
+    $.ajax({
+        //url du form. avec attr = attribut action
+        url:$form.attr('action'),
+        //objet avec arg.qui fait réf à form html
+        data:new FormData($form[0]),
+        //ne pas traiter les données
+        processData: false,
+        //ne pas configurer le contentType
+        contentType: false,
+        method:"post"
+
+    //afficher msg lors de l'évènement
+    //dès que la requête ajax a réussi
+    }) .done(function(){
+        //trouver le §p avec id msg et ajouter le texte du msg
+        $form.find('p#msg').text('Votre description de besoin a bien été enregistrée.');
+    })
 });
